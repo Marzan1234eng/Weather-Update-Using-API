1b329b8315c51424d6afdfe7a7df353b63a44c56
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-08-19 19:27:54 +0600

    temperature metric=celcius added

--- js/weather.api.js
+++ js/weather.api.js
@@ -6,7 +6,7 @@ let weather = document.getElementById('weather');
 const btn = document.getElementById('btn');
 btn.addEventListener('click', function (){
 
-    let cityNameUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search.value + "&appid=f62925ad62070148cd890732bc1f1891";
+    let cityNameUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search.value + "&units=metric" + "&appid=f62925ad62070148cd890732bc1f1891";
     console.log(cityNameUrl);
     fetch(cityNameUrl)
         .then(result => result.json())
